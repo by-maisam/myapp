369fe2d79ddec7e4f0dce8e714b73e4d2033b813
by-maisam <maisamraz473@gmail.com>
2026-05-01 10:11:55 -0700

    Declared Dimensions and Spacings

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,18 @@
+class Dimensions {
+  static const double paddingSmall = 8.0;
+  static const double paddingMeidum = 16.0;
+  static const double paddingLarge = 24.0;
+
+  static const double MarginSmall = 8.0;
+  static const double MarginMeidum = 16.0;
+  static const double MarginLarge = 24.0;
+
+  static const double borderRadiusSmall = 8.0;
+  static const double borderRadiusMeidum = 16.0;
+  static const double borderRadiusLarge = 24.0;
+  static const double borderRadiusCard = 8.0;
+
+  static const double spacingSmall = 8.0;
+  static const spacingMedium = 16.0;
+  static const double spacingLarge = 24.0;
+}
---
+++ lib/utils/spacing.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/utils/dimensions.dart';
+
+class Spacing {
+  static const SizedBox smallY = SizedBox(height: Dimensions.spacingSmall);
+  static const SizedBox mediumY = SizedBox(height: Dimensions.spacingMedium);
+  static const SizedBox largeY = SizedBox(height: Dimensions.spacingLarge);
+
+  static const SizedBox smallX = SizedBox(width: Dimensions.spacingSmall);
+  static const SizedBox mediumX = SizedBox(width: Dimensions.spacingMedium);
+  static const SizedBox largeX = SizedBox(width: Dimensions.spacingLarge);
+}
